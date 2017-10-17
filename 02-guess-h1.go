8f6bc8a2563f62922a1e4bd697f49785031d6144
rayman51 <ray.mannionit2@gmail.com>
2017-10-17 20:26:59 +0100

    added h1 tag

---
+++ 02-guess-h1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1>Guessing Game!</h1>")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
